55a55210a1c64888060fd736a33c96640d5315ed
pmudassir <pmuddasir@gmail.com>
2024-07-20 21:17:08 +0300

    clerk auth done

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 type Props = {};
@@ -9,8 +10,14 @@ const Document = (props: Props) => {
     <div>
       <Header>
         <div className="flex w-fit items-center justify-center gap-2">
-          <p className="document-title">This is a fake document title</p>
+          <p className="document-title">Share</p>
         </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </Header>
       <Editor />
     </div>
