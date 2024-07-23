27ab00461e2d66759e7a6d027626f11edb96231f
pmudassir <pmuddasir@gmail.com>
2024-07-23 09:44:03 +0300

    notification done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
 import AddDocumentBtn from "@/components/AddDocumentBtn";
 import DeleteModal from "@/components/DeleteModal";
 import Header from "@/components/Header";
+import Notifications from "@/components/Notifications";
 import { getDocuments } from "@/lib/actions/room.actions";
 import { dateConverter } from "@/lib/utils";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
-
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
@@ -22,7 +22,7 @@ const Home = async () => {
     <main className="home-container">
       <Header className="sticky left-0 top-0">
         <div className="flex items-center gap-2 lg:gap-4">
-          Notification
+          <Notifications />
           <SignedIn>
             <UserButton />
           </SignedIn>
